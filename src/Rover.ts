7640ee458e5901fa5444f6e65a05bd6280c78015
Sylvain Yvonneau <sylvain.yvonneau@lacombedulionvert.fr>
2023-04-04 15:02:58 +0200

    generate default Rover

--- src/Rover.ts
+++ src/Rover.ts
@@ -1,3 +1,10 @@
+import { Orientation } from "./Orientation";
+
 export class Rover {
-    
+
+
+    getPosition() {
+        return {x: 0, y: 0, orientation : Orientation.North};
+    }
+
 }
--- tests/Rover.spec.ts
+++ tests/Rover.spec.ts
@@ -1,1 +1,14 @@
 import {Rover} from "../src/Rover"
+import {Orientation} from "../src/Orientation"
+
+describe( 'Rover', () => {
+
+    
+    it('should be positionned at x=0 and y=0 and face North if no entry is provided', function () {
+        let rover = new Rover();
+
+        const actual = rover.getPosition();
+
+        expect(actual).toEqual({x: 0, y: 0, orientation: Orientation.North});
+    });
+});
